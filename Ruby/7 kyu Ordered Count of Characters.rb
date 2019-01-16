9c1c75db17eb9d30582927788d19c4d46fc065a3
forijdo <forijdo@gmail.com>
2019-01-16 17:12:14 +0300

    Ruby\7 kyu Ordered Count of Characters

---
+++ Ruby/7 kyu Ordered Count of Characters.rb
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/reviews/5a3abc231c1761dfc50017ad/groups/5c3f3a2220c1e70001196eea
+
+def ordered_count(s)
+    a = Hash.new(0)
+    s.chars.map{ |e| a[e] += 1 }
+    a.to_a
+end
